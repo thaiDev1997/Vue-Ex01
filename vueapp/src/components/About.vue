e20ff4943abc2812cfba5bb3a03d5108ef2f78b2
= <huynhvanthai.developer@gmail.com>
2018-06-11 23:14:40 +0700

    Ex01 Vue.js

---
+++ vueapp/src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="about">
+        <h1>Hello {{their_name}}, This is an about page</h1>
+        <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem quod veritatis reprehenderit quibusdam. Quisquam fuga culpa veniam ad aliquid quas ipsa ullam, provident expedita totam officia incidunt rem fugiat nobis? </p>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "Skill",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
+
